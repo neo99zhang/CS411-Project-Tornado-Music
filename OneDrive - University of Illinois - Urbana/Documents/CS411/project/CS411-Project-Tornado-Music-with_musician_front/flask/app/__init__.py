2f96deb1ba7d541bc76ab9fbe82dacc5cb791539
neo99zhang <Feiyu.17@intl.zju.edu.cn>
2021-12-01 16:15:19 -0600

    now both sql and app running on cloud

--- OneDrive - University of Illinois - Urbana/Documents/CS411/project/CS411-Project-Tornado-Music-with_musician_front/flask/app/__init__.py
+++ OneDrive - University of Illinois - Urbana/Documents/CS411/project/CS411-Project-Tornado-Music-with_musician_front/flask/app/__init__.py
@@ -6,6 +6,53 @@ from yaml import load, Loader
 
 
 
+#db.py
+import os
+import pymysql
+from flask import jsonify
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+# something like https://cloud.google.com/secret-manager/docs/overview to help keep
+# secrets secret.
+
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_password,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    db_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        #**db_config
+    )
+    # unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    # try:
+    #     if os.environ.get('GAE_ENV') == 'standard':
+    #         conn = pymysql.connect(user=db_user, password=db_password,
+    #                             unix_socket=unix_socket, db=db_name,
+    #                             cursorclass=pymysql.cursors.DictCursor
+    #                             )
+    # except pymysql.MySQLError as e:
+    #     print(e)
+
+    return pool
+
+
 def init_connection_engine():
     """ initialize database setup
     Takes in os variables from environment if on GCP
@@ -41,8 +88,8 @@ def init_connection_engine():
 
 
 app = Flask(__name__)
-db = init_connection_engine()
-
+# db = init_connection_engine()
+db = open_connection()
 
 # To prevent from using a blueprint, we use a cyclic import
 # This also means that we need to place this import here
