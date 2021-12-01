071d855107828085acf2204705c9ccaf82b4212b
Kaiwen <kaiwen@66.253.214.16>
2021-12-01 18:53:37 +0800

    update front

--- flask/app/database.py
+++ flask/app/database.py
@@ -1,5 +1,102 @@
 """Defines all the functions related to the database"""
 from app import db
+from flask_login import UserMixin
+
+class User(UserMixin):
+    pass
+
+
+
+def like(flag:int,uid:int)->None:
+    print('abcd')
+    conn=db.connect()
+    user_id=1
+    flag=1
+    print('abc')
+    #query_if_exist = 'select * from LIKE_LIST WHERE User_id='+ str(user_id) +' AND Music_id='+str(flag)+';'
+
+    # print(conn.execute(query_if_exist).fetchall()==[])
+    # print("3")
+
+    # if(conn.execute(query_if_exist).fetchall()==[]):
+    #     query='INSERT INTO LIKE_LIST VALUES ('+ str(user_id)+','+str(flag)+','+'0);'
+    # else:
+    #     query='UPDATE LIKE_LIST SET How_much=How_much+1 WHERE User_id='+ str(user_id) +' AND Music_id='+str(flag)+';'
+    query='INSERT INTO LIKE_LIST VALUES ('+ str(user_id)+','+str(flag)+','+'0);'
+    # print(query)
+    conn.execute(query)
+    conn.close()
+    
+    
+def dislike(flag:int)->None:
+    #print('abc')
+    conn=db.connect()
+    
+    user_id=uid
+    
+    if(conn.execute(query_if_exist).fetchall()!=[]):
+        query='UPDATE LIKE_LIST SET How_much=How_much-1 WHERE User_id='+ str(user_id) +' AND Music_id='+str(flag)+';'
+    
+    # print("333")
+    # print(query)
+    conn.execute(query)
+    conn.close()
+    
+    
+    
+    
+    
+def musician(musician:str)->dict:
+    conn=db.connect()
+    query="SELECT Music_name FROM Musician NATURAL JOIN  Produce JOIN Music USING(Music_id_spotify) WHERE musician_name='"+musician+"' LIMIT 30;"
+    print(query)
+    conn = db.connect()
+ 
+    query_results = conn.execute(query).fetchall()
+    print([x for x in query_results])
+    conn.close()
+ 
+    # print(query_results)
+    items1=[{}]
+    for i,result in enumerate(query_results):
+        item={
+            "id":i,
+            "Music_name": result[0],
+           
+            "musician_name": musician
+            
+        }
+        items1.append(item)
+    print(items1)
+    return items1
+
+
+# def signup_user(user:str, password:str) -> None:
+    
+#     conn = db.connect()
+#     query_results = conn.execute("Select count(*) from User ").fetchall()
+#     print(query_results[0][0])
+#     id=query_results[0][0]
+#     query = 'Insert Into User (User_id,User_password,User_nickname) VALUES ("{}", "{}","{}");'.format(
+#         id,password,user)
+#     print(query)
+#     conn.execute(query)
+#     conn.close()
+    
+    
+# def query_user(user_id:str)->dict:
+#     conn = db.connect()
+#     query_results = conn.execute("Select * from User WHERE User_nickname='"+ user_id+"' LIMIT 10;").fetchall()
+#     conn.close()
+#     user = None
+
+#     for result in query_results:
+#         user={'id':result[0],'username':result[2],'password':result[1]}
+    
+   
+
+#     return user
+        
 
 def fetch_todo() -> dict:
     """Reads all tasks listed in the todo table
@@ -9,7 +106,7 @@ def fetch_todo() -> dict:
     """
 
     conn = db.connect()
-    query_results = conn.execute("Select * from Music limit 10;").fetchall()
+    query_results = conn.execute("Select * from Music limit 30;").fetchall()
     query_results1 = conn.execute("Select * from TAG;").fetchall()
     conn.close()
     todo_list = []
@@ -213,3 +310,53 @@ def procedure(input_user_id: int) -> dict:
         items1.append(item)
 
     return items1
+
+def sign_up(username:str,password:str) -> int:
+    if len(username) > 10 or len(password)>10:
+        print('password or username too long')
+        return -1
+
+    res = 0
+    for x in username:
+        res*=256
+        res+= ord(x)
+    res=res%(2**32)
+    conn = db.connect()
+    query = 'select * from User WHERE User_id = '+ str(res) + ';'
+    query_results = conn.execute(query).fetchall()
+    if  not len(query_results) == 0:
+        print("user already exist")
+        return -1
+
+    query = 'insert into User VALUES  ("{}", "{}","{}");'.format(res,password,username)
+    conn.execute(query)
+    conn.close()
+    return 1
+
+
+
+def log_in(username:str,password:str) -> int:
+    success = -1
+    if len(username) > 10 or len(password)>10:
+        print('password or username too long')
+        return -1
+    res = 0
+    for x in username:
+        res*=256
+        res+= ord(x)
+    res=res%(2**32)
+    conn = db.connect()
+    query = 'select * from User WHERE User_id = '+ str(res) + ' AND User_nickname = \''+username+'\';'
+    query_results = conn.execute(query).fetchall()
+    if len(query_results) == 0:
+        print("user_name not exit")
+    if len(query_results) > 1:
+        print("unexpected error")
+    if query_results[0]['User_password'] == password:
+        user_id=res;
+        print("login success!")
+        success = 1
+    else:
+        print("password error!")
+        res = -1
+    return success,res
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,7 +1,7 @@
 """ Specifies routing for the application"""
 from flask import render_template, request, jsonify, redirect, url_for
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
-from models import query_user, User
+# from models import query_user, User
 from app import app
 from app import database as db_helper
 from flask_bootstrap import Bootstrap
@@ -15,6 +15,32 @@ def delete(task_id):
     try:
         db_helper.remove_task_by_id(task_id)
         result = {'success': True, 'response': 'Removed task'}
+        app.logger.info('deleting')
+    except:
+        result = {'success': False, 'response': 'Something went wrong'}
+
+    return jsonify(result)
+
+
+@app.route("/like/<int:flag>", methods=['POST'])
+def like(flag):
+    """ recieved post requests for entry delete """
+    try:
+        uid=1  ### needs to be removed if log 
+        db_helper.like(flag,uid)
+        result = {'success': True, 'response': 'LIKE task','flag':flag}
+    except:
+        result = {'success': False, 'response': 'Something went wrong','flag':flag}
+
+    return jsonify(result)
+
+@app.route("/dislike/<int:flag>", methods=['POST'])
+def dislike(flag):
+    """ recieved post requests for entry delete """
+
+    try:
+        db_helper.dislike(flag,uid)
+        result = {'success': True, 'response': 'LIKE task'}
     except:
         result = {'success': False, 'response': 'Something went wrong'}
 
@@ -102,53 +128,108 @@ def storedProc():
     print(items4)
     return render_template("like_score.html",items1=items4)
 
-app.secret_key = '1234567' # random
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
-login_manager.login_message = 'Please Login'
-login_manager.init_app(app)
+# app.secret_key = '1234567' # random
+# login_manager = LoginManager()
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
+# login_manager.login_message = 'Please Login'
+# login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    if query_user(user_id) is not None:
-        curr_user = User()
-        curr_user.id = user_id
+# @login_manager.user_loader
+# def load_user(user_id):
+#     if db_helper.query_user(user_id) is not None:
+#         curr_user = db_helper.User()
+#         curr_user.id = user_id
 
-        return curr_user
+#         return curr_user
 
-@app.route('/login', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         user_id = request.form.get('userid')
+#         user = db_helper.query_user(user_id)
+#         if user is not None and request.form['password'] == user['password']:
+
+#             curr_user = db_helper.User()
+#             curr_user.id = user_id
+
+#             login_user(curr_user)
+
+#             return 'Login successfully'
+
+#         #flash('Wrong username or password!')
+
+#     return render_template('login.html')
+
+
+
+# @app.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     return 'Logged out successfully!'
+
+# @app.route('/signin',methods=['GET', 'POST'])
+# def signin():
+#     if request.method == 'POST':
+#         user= request.form.get('Username')
+#         password=request.form['password']
+#         if user is not None and password is not None:
+#             db_helper.signup_user(user,password)
+#     return render_template('signin.html')
+
+@app.route("/login", methods=['POST','GET'])
 def login():
-    if request.method == 'POST':
-        user_id = request.form.get('userid')
-        user = query_user(user_id)
-        if user is not None and request.form['password'] == user['password']:
+    """ recieved post requests for entry updates """
+    global items6,items7,uid
+    print("we are here!")
+    data = request.get_json() 
+    fff = -1
+    if data!=None:
+        items6 = data['username']
+        items7 =data['password']
+        fff,uid = db_helper.log_in(data['username'],data['password'])
+    result = {'success': True, 'response': 'DONE'}
+    print(fff,uid)
+    # return jsonify(data)
+    return jsonify(result)
 
-            curr_user = User()
-            curr_user.id = user_id
-
-            login_user(curr_user)
-
-            return 'Login successfully'
-
-        #flash('Wrong username or password!')
-
-    return render_template('login.html')
+@app.route("/signup", methods=['POST','GET'])
+def signup():
+    """ recieved post requests for entry updates """
+    global items8,items9
+    data = request.get_json()
+    fff = -1
 
 
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return 'Logged out successfully!'
+    if data!=None:
+        items8 = data['username']
+        items9 =data['password']
+        fff = db_helper.sign_up(data['username'],data['password'])
+    result = {'success': True, 'response': 'DONE'}
+    # return jsonify(data)
+    return jsonify(result)
 
-@app.route('/signin')
-def signin():
+
+
+
+@app.route("/music", methods=['GET', 'POST'])
+def music():
+    """ recieves post requests to add new task """
+    result = {'success': 0, 'response': "DONE"}
+    print(1111111)
+    data = request.get_json()
     
-    return render_template('signin.html')
-
-
+    global items5
+    if data!=None:
+        items5 = db_helper.musician(data['search'])
+        result = {'success': True, 'response': 'Status Updated'}
+    
+    # return render_template("like_score.html",items1=items4)
+    # db_helper.insert_new_task(data['task'])
+  
+    return render_template('music.html',items2=items5)
 
 
 @app.route("/")
@@ -156,3 +237,16 @@ def homepage():
     """ returns rendered homepage """
     items = db_helper.fetch_todo()
     return render_template("index.html", items=items)
+
+@app.route("/temp")
+def temp_homepage():
+    """ returns rendered homepage """
+    
+    return render_template("index_temp.html", items=items)
+
+
+@app.route("/about")
+def about():
+    """ returns rendered homepage """
+    
+    return render_template("about.html", items=items)
